99926b433df2d57a5c0dc53f88db8240419078b5
Darshana <keerthi.mac@gmail.com>
2023-11-20 08:41:03 +0530

    course Work 1/4

--- Programming Fundamentals/week_02/Course_Work_02/Example.java
+++ Programming Fundamentals/week_02/Course_Work_02/Example.java
@@ -1,25 +1,35 @@
 class Example{
 	public static void main(String args[]){
+		System.out.println("\n");
 		System.out.println("\t==================================================================================================");
 		System.out.println("\t||\t\t\t\t\t\t\t\t\t\t\t\t||");
 		System.out.println("\t||\t\t\t\t\t\t\t\t\t\t\t\t||");
-		System.out.println("\t||\t\t\t\t                     =*%@@%*=\t\t\t\t\t||");
-		System.out.println("\t||\t\t\t\t                   =@@@@@@@@@@=\t\t\t\t\t||");
-		System.out.println("\t||\t\t\t\t    .+#@@#+.      =@@@@@@@@@@@@=\t\t\t\t||");
-		System.out.println("\t||\t\t\t\t   :@@@@@@@@-     @@@@@@@@@@@@@%\t\t\t\t||");
-		System.out.println("\t||\t\t\t\t   @@@@@@@@@@     =@@@@@@@@@@@@=\t\t\t\t||");
-		System.out.println("\t||\t\t\t\t   :@@@@@@@@-      +@@@@@@@@@@=\t\t\t\t\t||");
-		System.out.println("\t||\t\t\t\t    .+#@@#+.         =*%@@%*=\t\t\t\t\t||");
-		System.out.println("\t||\t\t\t\t  =#@@@@@@@@@@@@@@@@@@@@@@@@@@@#=\t\t\t\t||");
-		System.out.println("\t||\t\t\t\t *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*       -+%@#\t\t\t||");
-		System.out.println("\t||\t\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  :+#@@@@@@@\t\t\t||");
-		System.out.println("\t||\t\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\t\t\t||");
-		System.out.println("\t||\t\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\t\t\t||");
-		System.out.println("\t||\t\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\t\t\t||");
-		System.out.println("\t||\t\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\t\t\t||");
-		System.out.println("\t||\t\t\t\t@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  :+#@@@@@@@\t\t\t||");
-		System.out.println("\t||\t\t\t\t *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*       -+%@#\t\t\t||");
-		System.out.println("\t||\t\t\t\t  =#@@@@@@@@@@@@@@@@@@@@@@@@@@@#=\t\t\t\t||");
+		System.out.println("\t||\t\t\t\t                       =*%@@%*=\t\t\t\t\t||");
+		System.out.println("\t||\t\t\t\t                     =@@@@@@@@@@=\t\t\t\t||");
+		System.out.println("\t||\t\t\t\t      .+#@@#+.      =@@@@@@@@@@@@=\t\t\t\t||");
+		System.out.println("\t||\t\t\t\t     :@@@@@@@@-     @@@@@@@@@@@@@%\t\t\t\t||");
+		System.out.println("\t||\t\t\t\t     @@@@@@@@@@     =@@@@@@@@@@@@=\t\t\t\t||");
+		System.out.println("\t||\t\t\t\t     :@@@@@@@@-      +@@@@@@@@@@=\t\t\t\t||");
+		System.out.println("\t||\t\t\t\t      .+#@@#+.         =*%@@%*=\t\t\t\t\t||");
+		System.out.println("\t||\t\t\t\t    =#@@@@@@@@@@@@@@@@@@@@@@@@@@@#=\t\t\t\t||");
+		System.out.println("\t||\t\t\t\t   *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*       -+%@#\t\t||");
+		System.out.println("\t||\t\t\t\t  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  :+#@@@@@@@\t\t||");
+		System.out.println("\t||\t\t\t\t  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\t\t||");
+		System.out.println("\t||\t\t\t\t  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\t\t||");
+		System.out.println("\t||\t\t\t\t  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\t\t||");
+		System.out.println("\t||\t\t\t\t  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\t\t||");
+		System.out.println("\t||\t\t\t\t  @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@  :+#@@@@@@@\t\t||");
+		System.out.println("\t||\t\t\t\t   *@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@*       -+%@#\t\t||");
+		System.out.println("\t||\t\t\t\t    =#@@@@@@@@@@@@@@@@@@@@@@@@@@@#=\t\t\t\t||");
+		System.out.println("\t||\t\t\t\t             .%@@@++@@@%:          \t\t\t\t||");
+		System.out.println("\t||\t\t\t\t            =@@@%.  .%@@@=         \t\t\t\t||");
+		System.out.println("\t||\t\t\t\t            =%%=      =%%=         \t\t\t\t||");
+		System.out.println("\t||\t\t\t\t\t\t\t\t\t\t\t\t||");;
+		System.out.println("\t||\t\t\t\t   ________________________________________________\t\t||");
+		System.out.println("\t||\t\t\t\t  / _ \\/_  __/  __/ / ___/   _/ \\/ / __/  \\/  / _ |\t\t||");
+		System.out.println("\t||\t\t\t\t / , _/ / / _\\ \\   / /___ / //    / _// /\\_/ / __ |\t\t||");
+		System.out.println("\t||\t\t\t\t/_/\\_\\ /_/ /___/   \\____/___/_/\\_/___/ /  / /_/ |_|\t\t||");
+		System.out.println("\t||\t\t\t\t\t\t\t\t\t\t\t\t||");
 		System.out.println("\t==================================================================================================");
 				
 	}
