1fb14d8917fd1a251bdaa213839bf4382f0c6c73
Darshana <keerthi.mac@gmail.com>
2023-11-27 21:44:38 +0530

    PRF course 3 Chart

--- Programming Fundamentals/week_03/course_work_03/Example.java
+++ Programming Fundamentals/week_03/course_work_03/Example.java
@@ -13,14 +13,11 @@ class Example {
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t  _____      _                 _   _____ _                     ");
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t / ____|    | |               | | |_   _| |                    ");
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t| (___   ___| |__   ___   ___ | |   | | | |_ ___ _ __ ___  ___ ");
-        System.out.println(
-                "\t\t\t\t\t\t\t\t\t\t\t\t \\___ \\ / __| '_ \\ / _ \\ / _ \\| |   | | | __/ _ \\ '_ ` _ \\/ __|");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t \\___ \\ / __| '_ \\ / _ \\ / _ \\| |   | | | __/ _ \\ '_ ` _ \\/ __|");
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t ____) | (__| | | | (_) | (_) | |  _| |_| ||  __/ | | | | \\__ \\");
-        System.out.println(
-                "\t\t\t\t\t\t\t\t\t\t\t\t|_____/ \\___|_| |_|\\___/ \\___/|_| |_____|\\__\\___|_| |_| |_|___/");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t|_____/ \\___|_| |_|\\___/ \\___/|_| |_____|\\__\\___|_| |_| |_|___/");
         System.out.println("");
-        System.out.println(
-                "\t\t\t\t\t\t\t\t\t\t\t=============================================================================");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t=============================================================================");
 
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t   ____   __  __ _            _____ _                     ");
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t  / __ \\ / _|/ _(_)          |_   _| |                    ");
@@ -29,8 +26,7 @@ class Example {
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t | |__| | | | | | | (_|  __/  _| |_| ||  __/ | | | | \\__ \\");
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t  \\____/|_| |_| |_|\\___\\___| |_____|\\__\\___|_| |_| |_|___/");
         System.out.println("");
-        System.out.println(
-                "\t\t\t\t\t\t\t\t\t\t\t=============================================================================");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t=============================================================================");
 
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t   ____  _   _                 _____ _                     ");
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t  / __ \\| | | |               |_   _| |                    ");
@@ -39,13 +35,10 @@ class Example {
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t | |__| | |_| | | |  __/ |     _| |_| ||  __/ | | | | \\__ \\");
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t  \\____/ \\__|_| |_|\\___|_|    |_____|\\__\\___|_| |_| |_|___/");
         System.out.println("");
-        System.out.println(
-                "\t\t\t\t\t\t\t\t\t\t\t=============================================================================");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t\t=============================================================================");
 
-        System.out.println(
-                "\t\t\t\t\t\t\t\t\t\t=============================================================================");
-        System.out.println(
-                "\t\t\t\t\t\t\t\t\t\t+--------------------------------------------------------------------------------+");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t=============================================================================");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t+--------------------------------------------------------------------------------+");
         System.out.println("\t\t\t\t\t\t\t\t\t\t|\t\t______             _      _____ _                                |");
         System.out.println("\t\t\t\t\t\t\t\t\t\t|\t\t| ___ \\           | |    /  ___| |                               |");
         System.out.println("\t\t\t\t\t\t\t\t\t\t|\t\t| |_/ / ___   ___ | | __ \\ `--.| |__   ___  _ __                 |");
@@ -55,6 +48,45 @@ class Example {
         System.out.println("\t\t\t\t\t\t\t\t\t\t|\t\t                                           | |                   |");
         System.out.println("\t\t\t\t\t\t\t\t\t\t|\t\t                                           |_|                   |");
         System.out.println("\t\t\t\t\t\t\t\t\t\t+--------------------------------------------------------------------------------+");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t| Tel  : %-70s  | %n","0753221031");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t| Name : %-54s Date :%-10s | %n","Piyal","13-11-2023");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t+--------------------------------------------------------------------------------+");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","Items","QTY","Unit Price","Price");
+        System.out.println("\t\t\t\t\t\t\t\t\t\t+--------------------------------------------------------------------------------+");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);        
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7d |      %-15.2f |      %-12.2f |%n","CR Book",5,80.00,400.00);
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s |      %-15s |      %-12s |%n","","","","");        
+        System.out.println("\t\t\t\t\t\t\t\t\t\t+--------------------------------------------------------------------------------+"); 
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s        %-15s |      %-12.2f |%n","","Total","",400.00);
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|---------------------------------------------------------+%n","");        
+		System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s %-15s       |      %-12.2f |%n","","Discount","(10%)",400.00);
+        System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|---------------------------------------------------------+%n",""); 		
+		System.out.printf("\t\t\t\t\t\t\t\t\t\t|       %-15s|      %-7s        %-15s |      %-12.2f |%n","","Price","",400.00); 		                                
+        System.out.println("\t\t\t\t\t\t\t\t\t\t+--------------------------------------------------------------------------------+");             
 
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t  ____        _                      ");
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t |  _ \\      | |                     ");
@@ -63,6 +95,7 @@ class Example {
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t | |_) | (_| | | (_| | | | | (_|  __/");
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t\t |____/ \\__,_|_|\\__,_|_| |_|\\___\\___|");
         System.out.println("\t\t\t\t\t\t\t\t\t\t\t\t\t==================================================");
+             
 
     }
 }
