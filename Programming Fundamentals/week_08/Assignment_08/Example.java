1d8366dc3bb5d33f4b3a1a62f830ec40970694b7
keerthimac <keerthi.mac@gmail.com>
2024-01-10 18:41:28 +0530

    Ass 08 PRF

--- Programming Fundamentals/week_08/Assignment_08/Example.java
+++ Programming Fundamentals/week_08/Assignment_08/Example.java
@@ -3,8 +3,7 @@ class Example{
 	public static void main(String args[]){
 		String[] months = {"January", "February", "March", "April", "May", "June", "July", "August",
 		"September", "October", "November", "December"};
-		int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
-		
+		int[] daysInMonth = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};	
 		int[][] patientCounts = new int[12][];
 		
 		Random r = new Random();
@@ -18,45 +17,28 @@ class Example{
 				patientCounts[i][j]=r.nextInt(501);
 			}
 		}
-
-		for (int i = 0; i < months.length; i++){
-			for (int j = 0; j < daysInMonth[i]; j++){
-				System.out.print(patientCounts[i][j]+" ");
-			}
-		System.out.println();
-		}
-
-		System.out.println();
-		System.out.println();
-
-
-		for (int i = 0; i <31 ; i++){
-			System.out.print(i+1+"   ");
-			for (int j = 0; j <12 ; j++){
-				System.out.print(patientCounts[j][i]+"\t");
-			}
-		System.out.println();
-		}
-
-/*
-
-		for (int i = 0; i < months.length; i++){
-			//System.out.printf("%-5s"," ",months[i]);
-			System.out.printf("    %-5s",months[i]);
-		}
-		System.out.println();
-
+		System.out.println("-------------------------------------------------------------------------------------------------------------");
+		System.out.println("\t\t\t\tCOVID-19 patient in 2021");
+		System.out.println("-------------------------------------------------------------------------------------------------------------");
+		System.out.println("\n\n\n");
 		
+		System.out.printf("%-5s","");
+		for (int i = 0; i < months.length; i++){
+			System.out.printf("%-10s",months[i]);
+		}
+		System.out.println();
 
-		for (int i = 0; i <31 ; i++){
-			System.out.printf("%-5s",i+1);
-			//System.out.print(i+1+"   ");
-			for (int j = 0; j <12; j++){
-				System.out.printf("%-10s",patientCounts[i].length);
+		for (int i = 0; i <31 ; i++){			
+			System.out.printf("%-5d",i+1);			
+			for (int j = 0; j <12 ; j++){	
+				if ((i==28&&j==1)||(i==29&&j==1)||(i==30&&j==1)||(i==30&&j==3)||(i==30&&j==5)||(i==30&&j==8)||(i==30&&j==10)){
+					System.out.printf("%-10s","");
+				}else{
+					System.out.printf("%-10s",patientCounts[j][i]);
+				}
 			}
 		System.out.println();
 		}
-*/
 
 	}
 }
