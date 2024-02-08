0e009ca922f3ce6e2ec82c93e6347de3f23f0141
Darshana <keerthi.mac@gmail.com>
2024-02-08 09:09:52 +0530

    OOP Ass Submitted

--- Object Oriented Programming/week_02/assignment_02/04/Demo.java
+++ Object Oriented Programming/week_02/assignment_02/04/Demo.java
@@ -1,14 +1,66 @@
 class Registry{
-	private nextIndex;
+	private int nextIndex;
 	private int[] dataArray;
 	
 	Registry(int size){
 		nextIndex=0;
 		dataArray = new int[size];
 	}
+	public void add(int num){
+		dataArray[nextIndex++]=num;
+	}
+
+	public void add(int index, int num){
+		for (int i = nextIndex-1; i >= index; i--){
+			dataArray[i+1]=dataArray[i];
+		}
+		dataArray[index]=num;
+		nextIndex++;
+	}
+
+	public void add(int[] arr){
+		for (int i = 0; i < arr.length; i++){
+			add(arr[i]);
+		}
+			
+	}
+
+	public void add(int index, int[] arr){
+		for (int i = arr.length-1; i >=0; i--){
+			add(index, arr[i]);
+		}
+	}
+	
+	public void remove(){
+		for (int i = 0; i<nextIndex; i++){
+			dataArray[i]=dataArray[i+1];
+		}
+		nextIndex--;
+	}
+
+	public void remove(int index){
+		for (int i = index; i<nextIndex; i++){
+			dataArray[i]=dataArray[i+1];
+		}
+		nextIndex--;
+	}
+
+	public void remove(int startIndex, int endIndex){
+		for (int i = endIndex-1; i >= startIndex; i--){
+			remove(i);
+		}
+		
+	}
+
+	public void printRegistry(){
+		System.out.print("[");
+		for (int i = 0; i < nextIndex; i++){
+			System.out.print(dataArray[i]+", ");
+		}
+		System.out.println(nextIndex<=0?"empty]":"\b\b]");
+	}
 	
 }
-
 
 
 class Demo{
