f25f81dc30e364b3c237fb9c8f9d27b2d0150eb6
Darshana <keerthi.mac@gmail.com>
2024-04-01 08:55:41 +0530

    Commit message

---
+++ Object Oriented Programming/week_09 JDBC/ThogaKade UpdateCustomerForm/ThogaKade/src/thogakade/ThogaKade.java
@@ -0,0 +1,23 @@
+package thogakade;
+
+import java.sql.*;
+
+public class ThogaKade {
+
+    public static void main(String[] args) {
+        String sql = "Insert into Customer Values('C1112','Niroth','Panadura',45000)";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException ex) {
+            System.out.println("Drvier s/w notg found...");
+        }
+        try {
+            Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root", "mysql");
+            Statement stm = conn.createStatement();
+            stm.executeUpdate(sql);
+        } catch (SQLException ex) {
+            System.out.println(ex.getMessage());
+        }
+    }
+
+}
---
+++ Object Oriented Programming/week_09 JDBC/ThogaKade/src/thogakade/db/DBConnection.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package thogakade.db;
+import java.sql.*;
+/**
+ *
+ * @author nirot
+ */
+public class DBConnection {
+    private static DBConnection dBConnection;
+    private Connection  connection;
+    private DBConnection() throws ClassNotFoundException, SQLException{
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        connection=DriverManager.getConnection("jdbc:mysql://localhost/ThogaKade", "root","mysql");
+    }
+    public static DBConnection getInstance() throws ClassNotFoundException, SQLException{
+        if(dBConnection==null){
+            dBConnection=new DBConnection();
+        }
+        return  dBConnection;
+    }
+    public Connection getConnection(){
+        return connection;
+    }
+}
