6e127506317f68bb3c0f896e45cb7a0f1ce8e737
Darshana Keerthirathna <keerthi.mac@gmail.com>
2024-05-28 08:48:04 +0530

    form initiated

--- Advance Web Application Dev/Day_11/product-frontend/src/app/app.routes.ts
+++ Advance Web Application Dev/Day_11/product-frontend/src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { Component } from '@angular/core';
 import { Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { RegistrationComponent } from './pages/registration/registration.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: 'register', component: RegistrationComponent },
+];
--- Advance Web Application Dev/Day_11/product-frontend/src/app/pages/registration/registration.component.ts
+++ Advance Web Application Dev/Day_11/product-frontend/src/app/pages/registration/registration.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './registration.component.html',
-  styleUrl: './registration.component.css'
+  styleUrl: './registration.component.css',
 })
 export class RegistrationComponent {
+  public product = {
+    name: undefined,
+    description: undefined,
+    category: undefined,
+    price: undefined,
+  };
 
+  onClick() {
+    console.log(this.product);
+  }
 }
